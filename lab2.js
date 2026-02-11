b221199a8a39ff1b76c64e1606b18611fb5bd632
khorunzhyirostyslav-collab <khorunzhyi.rostyslav@krfk.kai.edu.ua>
2026-02-11 13:51:42 +0200

    Add Lab 1 and Lab 2 files

---
+++ lab2.js
@@ -0,0 +1,43 @@
+// Оголошення змінних різними способами
+
+var studentName = "Rostyslav";
+let age = 16;
+const college = "Aviation College";
+
+console.log(studentName);
+console.log(age);
+console.log(college);
+// Завдання 2. Визначення типів змінних
+
+console.log(typeof studentName);
+console.log(typeof age);
+console.log(typeof college);
+// Завдання 3. Створення об'єкта
+
+let student = {
+    name: "Rostyslav",
+    age: 16,
+    isStudent: true
+};
+
+console.log(student);
+// Завдання 4. Додавання нового поля
+
+student.university = "Aviation College";
+
+console.log(student);
+// Завдання 5. Зміна типу змінної
+
+age = "16 років";
+
+console.log(age);
+console.log(typeof age);
+// Завдання 6. Спроба змінити const
+
+//college = "New College";
+
+console.log(college);
+
+
+
+
