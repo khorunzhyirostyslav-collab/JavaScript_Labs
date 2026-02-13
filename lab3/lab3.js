a6ea4488d34f6ddb9532c3cf1c8148e2f2406c80
khorunzhyirostyslav-collab <khorunzhyi.rostyslav@krfk.kai.edu.ua>
2026-02-13 15:05:40 +0200

    Lab3 task 1 completed

---
+++ lab3/lab3.js
@@ -0,0 +1,24 @@
+console.log("===== Завдання 1 =====");
+
+// Створення змінних
+let myString = "JavaScript";
+let myNumber = 25;
+let myBoolean = true;
+let myUndefined;
+let myNull = null;
+
+// Виведення значень
+console.log("Значення змінних:");
+console.log("myString =", myString);
+console.log("myNumber =", myNumber);
+console.log("myBoolean =", myBoolean);
+console.log("myUndefined =", myUndefined);
+console.log("myNull =", myNull);
+
+// Виведення типів
+console.log("Типи змінних:");
+console.log("typeof myString =", typeof myString);
+console.log("typeof myNumber =", typeof myNumber);
+console.log("typeof myBoolean =", typeof myBoolean);
+console.log("typeof myUndefined =", typeof myUndefined);
+console.log("typeof myNull =", typeof myNull);
