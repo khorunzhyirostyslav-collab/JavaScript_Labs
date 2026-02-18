b482441934c05d0e8988cd48b8b9fcda517b1da0
khorunzhyirostyslav-collab <khorunzhyi.rostyslav@krfk.kai.edu.ua>
2026-02-18 15:13:21 +0200

    Lab5: tasks 1-2

--- Lab4/lab4.js
+++ Lab4/lab4.js
@@ -1,1 +1,20 @@
-console.log("Лабораторна №4 запущена");
+// Завдання 1
+
+let temperature = 28;
+
+if (temperature < 10) {
+    console.log("Погода холодна ❄️");
+} else if (temperature >= 10 && temperature <= 25) {
+    console.log("Погода тепла 🌤");
+} else {
+    console.log("Погода спекотна 🔥");
+}
+// Завдання 2
+
+let number = 12;
+
+let result = (number % 2 === 0) 
+    ? "Число парне ✅" 
+    : "Число непарне ❌";
+
+console.log(result);
--- lab1.js
+++ lab1.js
@@ -1,5 +1,12 @@
-console.log("Hello, JavaScript!");
-console.log("Hello, JavaScript!");
-console.log("Lab 1 – JavaScript");
+// Завдання 1
 
+let temperature = 18;
+
+if (temperature < 10) {
+    console.log("Погода холодна ❄️");
+} else if (temperature >= 10 && temperature <= 25) {
+    console.log("Погода тепла 🌤");
+} else {
+    console.log("Погода спекотна 🔥");
+}
 
