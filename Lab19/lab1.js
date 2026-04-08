b0a9e7a435d89ff99c9b81bdbbd19b4b72d6ae19
khorunzhyirostyslav-collab <khorunzhyi.rostyslav@krfk.kai.edu.ua>
2026-04-08 09:19:43 +0300

    Lab19: реалізовано reduce

---
+++ Lab19/lab1.js
@@ -0,0 +1,12 @@
+// Завдання 1
+
+let temperature = 18;
+
+if (temperature < 10) {
+    console.log("Погода холодна ❄️");
+} else if (temperature >= 10 && temperature <= 25) {
+    console.log("Погода тепла 🌤");
+} else {
+    console.log("Погода спекотна 🔥");
+}
+
