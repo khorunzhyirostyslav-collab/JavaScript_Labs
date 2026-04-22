b8eb9663cb71a44b3c09c8c0f73aad3f9617cac0
khorunzhyirostyslav-collab <khorunzhyi.rostyslav@krfk.kai.edu.ua>
2026-04-22 11:53:33 +0300

    Підрахунок кількості символів у полі введення в реальному часі.

--- lab22/lab22.js
+++ lab22/lab22.js
@@ -1,6 +1,20 @@
-const colorSelect = document.getElementById("colorSelect");
-const colorBox = document.getElementById("colorBox");
+console.log("JS працює");
 
-colorSelect.addEventListener("change", function () {
-    colorBox.style.backgroundColor = colorSelect.value;
+const title = document.getElementById("title");
+
+title.addEventListener("mouseover", function () {
+    console.log("наведено");
+    title.style.color = "red";
+});
+
+title.addEventListener("mouseout", function () {
+    console.log("прибрано");
+    title.style.color = "black";
+});
+
+const input = document.getElementById("textInput");
+const count = document.getElementById("count");
+
+input.addEventListener("input", function () {
+    count.textContent = input.value.length;
 });
