d3005ada3cb0dff7d64eacc121587866357b5e67
khorunzhyirostyslav-collab <khorunzhyi.rostyslav@krfk.kai.edu.ua>
2026-05-04 16:42:32 +0300

    Додаткове завдання

--- lab24/lab24.js
+++ lab24/lab24.js
@@ -10,3 +10,24 @@ select.addEventListener("change", function(event) {
         result.textContent = "Ви обрали: " + value;
     }
 });
+// --- ДОДАТКОВЕ ЗАВДАННЯ ---
+const hoverText = document.getElementById("hoverText");
+const tooltip = document.getElementById("tooltip");
+
+// при наведенні
+hoverText.addEventListener("mouseover", function (event) {
+    tooltip.style.display = "block";
+    tooltip.style.left = event.pageX + "px";
+    tooltip.style.top = event.pageY + "px";
+});
+
+// при русі миші (щоб підказка рухалась)
+hoverText.addEventListener("mousemove", function (event) {
+    tooltip.style.left = event.pageX + "px";
+    tooltip.style.top = event.pageY + "px";
+});
+
+// коли курсор пішов
+hoverText.addEventListener("mouseout", function () {
+    tooltip.style.display = "none";
+});
